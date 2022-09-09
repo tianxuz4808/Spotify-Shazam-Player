7c83fc027ded17c91acd4e375d4877cedfc3f0ef
tianxuz4808 <tianxuz4808@gmail.com>
2022-09-09 19:43:39 -0400

    refactored code

--- MyShazam.py
+++ MyShazam.py
@@ -2,13 +2,13 @@ from Song import Song
 from shazamio import Shazam
 import asyncio
 
-class Shazam:
+class MyShazam:
     def __init__(self, audio_file) -> None:
         self.audio_file = audio_file
         pass
 
     async def shazam(self):
         shazam = Shazam()
-        song = await shazam.recongnize_song(self.audio_file)
+        song = await shazam.recognize_song(self.audio_file)
         print(song)
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from Shazam import Shazam
+from MyShazam import MyShazam
 import asyncio
 
 if __name__ == "__main__":
     import record
-    shazam = Shazam('testing.wav')
+    shazam = MyShazam('testing.wav')
     asyncio.run(shazam.shazam())
